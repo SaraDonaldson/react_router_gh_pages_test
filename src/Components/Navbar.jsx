bd4fd783342908c2ba33ab443077041088072132
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2023-09-02 17:54:24 +0100

    added router links to navbar for deployment test

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>Navbar
+
+    <Link to ='/'>Home</Link>
+    <Link to ='/dashboard'> Dashboard</Link>
+    <Link to ='/projects'> Projects</Link>
+    <Link to ='/profile'> About</Link>
+
+    </div>
   )
 }
 
